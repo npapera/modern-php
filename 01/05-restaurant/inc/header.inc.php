6d0a213aca9b1f8698a0eb972b9d23babb068893
npapera <ngere.papera@students.mq.edu.au>
2024-07-15 00:46:50 +1000

    moved styles folder location

--- 01/05-restaurant/inc/header.inc.php
+++ 01/05-restaurant/inc/header.inc.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/simple.css" />
-    <link rel="stylesheet" href="./styles/custom.css" />
+    <link rel="stylesheet" href="../../../styles/simple.css" />
+    <link rel="stylesheet" href="../../../styles/custom.css" />
     <title>Culinary Cove &bull; <?php echo $pageTitle; ?></title>
 </head>
 <body>
