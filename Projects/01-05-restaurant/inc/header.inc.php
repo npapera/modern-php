f1e45e2ed377194336e4d05ef0c7c6669ff65ef0
npapera <ngere.papera@students.mq.edu.au>
2024-07-16 14:13:27 +1000

    updated defaults for {pageTitle}, {headerImg} and {pageKey}

--- Projects/01-05-restaurant/inc/header.inc.php
+++ Projects/01-05-restaurant/inc/header.inc.php
@@ -5,30 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/simple.css" />
     <link rel="stylesheet" href="../../styles/custom.css" />
-    <title>Culinary Cove &bull; <?php echo $pageTitle; ?></title>
+    <title>Culinary Cove<?php if (!empty($pageTitle)): ?> &bull; <?php echo $pageTitle; ?><?php endif; ?></title>
 </head>
 <body>
-  <header class="header-with-background" style="background-image: url('<?php if(!empty($headerImg)): echo $headerImg; else: echo "images/pexels-engin-akyurt-1435904.jpg"; endif; ?>'); ">
+  <?php 
+    if (empty($headerImg)) {
+      $headerImg = 'images/pexels-engin-akyurt-1435904.jpg';
+    }
+    if (!isset($pageKey)) {
+      $pageKey = '';
+    }
+  ?>
+  <header class="header-with-background" style="background-image: url(<?php echo $headerImg; ?>); ">
     <h1>Culinary Cove</h1>
     <p>Your sanctuary for exceptional flavors</p>
     <nav>
-      <?php if(!empty($pageKey) && $pageKey === 'Mission'): ?>
-        <?php echo '<a class="active" href="our-mission.php">Our mission</a>'; ?>
-      <?php else: ?>
-        <?php echo '<a href="our-mission.php">Our mission</a>'; ?>
-      <?php endif; ?>
-
-      <?php if(!empty($pageKey) && $pageKey === 'Ingredients'): ?>
-        <?php echo '<a class="active" href="ingredients.php">Ingredients</a>'; ?>
-      <?php else: ?>
-        <?php echo '<a href="ingredients.php">Ingredients</a>'; ?>
-      <?php endif; ?>
-
-      <?php if(!empty($pageKey) && $pageKey === 'Menu'): ?>
-        <?php echo '<a class="active" href="menu.php">Our Menu</a>'; ?>
-      <?php else: ?>
-        <?php echo '<a href="menu.php">Our Menu</a>'; ?>
-      <?php endif; ?>
+      <a class="<?php if ($pageKey === 'Mission') echo 'active'; ?>" href="our-mission.php">Our mission</a>
+      <a class="<?php if ($pageKey === 'Ingredients') echo 'active'; ?>" href="ingredients.php">Our mission</a>
+      <a class="<?php if ($pageKey === 'Menu') echo 'active'; ?>" href="menu.php">Our mission</a>
     </nav>
   </header>
 
